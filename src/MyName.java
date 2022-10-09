1820cd7d3f1452e35b19bfc545e7ff206341d1bc
eklimkow <eklimkow@student.wszib.edu.pl>
2022-10-09 14:30:41 +0200

    update

--- src/MyName.java
+++ src/MyName.java
@@ -1,5 +1,8 @@
 public class MyName {
     public static void main(String[] args) {
         System.out.println("My name is Ewa.");
+        String level ="podyplomowych";
+        System.out.println("Ucze sie na studiach " + level + " na WSZIBie.");
+
     }
 }
